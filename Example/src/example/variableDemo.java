af5284dbbf3b2bfad19bb3efe0c3e9ade32292f6
THARUN675 <tharunmahapathro@gmail.com>
2025-08-06 15:59:44 +0530

    first commit

---
+++ Example/src/example/variableDemo.java
@@ -0,0 +1,15 @@
+package example;
+
+
+public class variableDemo {
+	
+	
+		String name="this is instance variable";
+		static String nametwo="this is static variable";
+		void print() {
+			String nametwo="this is from local variable";
+			System.out.println(nametwo);
+			
+		}
+	}
+
---
+++ Example/src/example/variableDemomain.java
@@ -0,0 +1,12 @@
+package example;
+
+public class variableDemomain {
+	
+	 public static void main(String[]args) {
+		variableDemo ob = new variableDemo ();
+		System.out.println (ob.name);
+		ob.print();
+		System.out.println(variableDemo.nametwo);
+	}
+
+}
